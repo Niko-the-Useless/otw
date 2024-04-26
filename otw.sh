559ec730f3c14213d9993840bbf6d30d5556cb46
Niko <radosz2003@gazeta.pl>
2024-04-26 10:16:26 +0200

    created repo

---
+++ otw.sh
@@ -0,0 +1,10 @@
+#! /bin/zsh
+if [ "$#" -eq 0 ]; then
+	content=$(cat /home/Niko/projects/bash/otw/.data)
+	pass=$(echo "$content"  | tail -1| awk '{print $3}')
+	level=$(echo "$content"  | head -1| awk '{print $3}')
+	echo "$pass" | xclip -selection clipboard
+	ssh -p 2220 $level
+else
+	echo "level = bandit$1@bandit.labs.overthewire.org\npass = $2" > /home/Niko/projects/bash/otw/.data
+fi
